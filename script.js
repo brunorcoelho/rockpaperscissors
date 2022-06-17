d474de106eed4d0f33ad781e82a36e20ef1fca39
Bruno Coelho <bruno090904@gmail.com>
2022-06-16 20:29:30 -0400

    Added in-HTML info about round/score

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ const array = ["rock", "paper", "scissors"];
 let playerPoints = 0;
 let computerPoints = 0;
 const buttonsBox = document.querySelector(".buttons-container");
+const gameInfo = document.querySelector(".game-info");
+const roundWinner = document.createElement("p");
+const score = document.createElement("p");
+const gameWinner = document.createElement("p");
 const restart = document.createElement("button");
+
 const handleClick = (e) => {
   // Calls the game function providing the Button ID and a random computer selection
   game(e.target.id, array[Math.floor(Math.random() * array.length)]);
 };
+
 // Listens for clicks on any of the three buttons on the page
 const buttons = document.querySelectorAll(".button");
 buttons.forEach((button) => {
@@ -34,17 +40,19 @@ function playRound(playerSelection, computerSelection) {
 
 // Handles the game until anyone scores 5 points
 function game(playerSelection, computerSelection) {
+  gameInfo.appendChild(roundWinner);
+  gameInfo.appendChild(score);
   let round = playRound(playerSelection, computerSelection);
   if (round == "PLAYER") {
-    alert(`Player wins.\nThe computer picked ${computerSelection}.`);
+    roundWinner.textContent = `Player wins the round. The computer picked ${computerSelection}.`;
     playerPoints++;
   } else if (round == "COMPUTER") {
-    alert(`Computer wins.\nThe computer picked ${computerSelection}.`);
+    roundWinner.textContent = `Computer wins the round. The computer picked ${computerSelection}.`;
     computerPoints++;
   } else {
-    alert(`It's a tie.`);
+    roundWinner.textContent = `It's a tie.`;
   }
-  console.log(`${playerPoints} x ${computerPoints}`);
+  score.textContent = `${playerPoints} x ${computerPoints}`;
   if (playerPoints == 5) {
     endGame("player");
   } else if (computerPoints == 5) {
@@ -54,7 +62,8 @@ function game(playerSelection, computerSelection) {
 
 // endGame() is called when the game detects someone made 5 points, then remove the Listener for the choices buttons
 function endGame(winner) {
-  alert(`The ${winner} won the game.`);
+  gameInfo.appendChild(gameWinner);
+  gameWinner.textContent = `The ${winner} won the game.`;
   buttons.forEach((button) => {
     button.removeEventListener("click", handleClick);
   });
@@ -63,10 +72,14 @@ function endGame(winner) {
   restart.addEventListener("click", restartGame);
 }
 
+// When the player clicks "Play Again?" the game resets the score, remove previous game info, and make the buttons listen again to play a new game.
 function restartGame() {
   playerPoints = 0;
   computerPoints = 0;
   buttonsBox.removeChild(restart);
+  gameInfo.removeChild(roundWinner);
+  gameInfo.removeChild(score);
+  gameInfo.removeChild(gameWinner);
   buttons.forEach((button) => {
     button.addEventListener("click", handleClick);
   });
